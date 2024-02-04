8ed365876db432aa58083827180078a074f81e0e
İSMAİL TAHA KORKUT <tahakorkutt@gmail.com>
2024-02-04 20:05:49 +0300

    HOTEL REST APİ
    HOTEL REST APİ

---
+++ Frontend/HotelProject.WebUI/Views/Shared/Components/Contact/_ContactCoverPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Views.Shared.Components.Contact
+{
+    public class _ContactCoverPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
